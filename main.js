5be40c65d833673652190620baf3a873073e34a7
Keslava-PL <kamdob1995@gmail.com>
2022-12-27 15:16:33 +0100

    added working engine

--- main.js
+++ main.js
@@ -2,29 +2,113 @@ const display = document.querySelector("#displayInput");
 const resetBtn = document.querySelector(".resetBtn");
 const spec = document.querySelectorAll(".spec");
 const number = document.querySelectorAll(".number");
+const equalSign = document.querySelector(".specEq");
 
-let value="";
-let temp="";
+let previousNumber = "";
+let currentNumber = "";
+let displayNumber = "";
+let operationSign = "";
 
-spec.forEach(function(sign){
-sign.addEventListener('click', function(){
-  clearDisplay();
-  doCalc(sign.value);
-});
-});
-number.forEach(function(one){
-  one.addEventListener('click', function(){
-    value = value + one.value;
-    display.value = value;
+
+let clickedSpec = 0;
+let score = 0;
+
+
+//event listener to operational buttons
+spec.forEach(function (par) {
+  par.addEventListener("click", function () {
+    operationSign = par.value;
+    
+    verify();
   });
 });
+//event listener to number buttons
+number.forEach(function (one) {
+  one.addEventListener("click", function () {
+    display.value = display.value + one.value;
+    currentNumber = display.value;
+    
+  });
+});
+function verify(){
 
-resetBtn.addEventListener('click', clearDisplay);
+  console.log("prev:",previousNumber, "cur:",currentNumber, "disp:",displayNumber);
+ 
+  if(previousNumber === "" && displayNumber ==="" ){
+    console.log('1');
+    previousNumber = currentNumber;
+    clearDisplay();
+  }
+  else if(previousNumber !== "" && displayNumber === "" && currentNumber!==""){
+    console.log('2');
+    doCalc(previousNumber, currentNumber, operationSign);
+    
+  }
+  else if(previousNumber != "" && currentNumber !="" && displayNumber !=""){
+    console.log('3');
+    //jezeli mam juz cur i prev i disp, nalezy wyczyscic display i ponowic dzialanie z nowo przyjetym curr
+    previousNumber = displayNumber;
+    clearDisplay();
+  }
+  else if(previousNumber!=="" && currentNumber ==="" & displayNumber ===""){
+    clearDisplay();
+  }
+}
+function stan(){
+  console.log("prev:",previousNumber, "cur:",currentNumber, "disp:",displayNumber);
+}
+function doCalc(pre, cur, sign){
+  if(sign === "+"){
+   displayNumber = add(Number(pre),Number(cur));
+  }
+  else if(sign === "-"){
+    displayNumber = sub(Number(pre),Number(cur));
+  }
+  else if(sign === "/"){
+   displayNumber=div(Number(pre),Number(cur));
+  }
+  else if(sign === "x"){
+   displayNumber=mul(Number(pre),Number(cur));
+  }
+  console.log(previousNumber, currentNumber, displayNumber);
 
-function clearDisplay(){
+  display.value = displayNumber;
+  previousNumber=displayNumber;
+  displayNumber = "";
+  currentNumber = "";
+}
+//event listener to equal button
+equalSign.addEventListener("click", verify);
+
+//event listener to Clear button
+resetBtn.addEventListener("click", function(){
+  previousNumber ="";
+  currentNumber="";
+  displayNumber="";
+  operationSign="";
+  clearDisplay();
+});
+
+//
+function clearDisplay() {
   display.value = "";
-  value = "";
-}
-function doCalc(sign){
 
+  currentNumber="";
+  displayNumber="";
 }
+
+
+
+function add(value1, value2) {
+  return value1 + value2;
+}
+function mul(value1, value2) {
+  return value1 + value2;
+}
+function div(value1, value2) {
+  return value1 / value2;
+}
+function sub(value1, value2) {
+  return value1 - value2;
+}
+
